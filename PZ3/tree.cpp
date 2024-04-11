8832324f7f33de624a947972183d392287264458
Артём Рубцов <artyomrubtsow@yandex.ru>
2024-04-12 03:07:09 +0700

    #ueee в 3 часа ночи очень тяжело прогать

--- PZ3/tree.cpp
+++ PZ3/tree.cpp
@@ -1,148 +1,156 @@
 #include<windows.h>
 #include"tree.h"
 
-tree *copy_tree( tree *root )
+
+enum place { LEFT = 0, RIGHT, ROOT };
+
+int tree::input( FILE *f )
 {
-   tree *t = NULL;
-   if ( root )
+   tree *root = NULL;
+
+   if ( fopen_s( &f, "in.txt", "r" ) )
    {
-      t = new tree( root->el );
-      stack *s1 = push( root, s1 ), *s2 = push( t, s2 );
-      for ( ; s1; )
-      {
-         tree *cur;
-         push( &cur, &s2 );
-         push( &root, &s1 );
-         if ( root->left )
-         {
-            cur->left = new tree( root->left->el );
-            s1 = push( root->left, s1 );
-            s2 = push( cur->left, s2 );
-         }
-         if ( root->right )
-         {
-            cur->right = new tree( root->right->el );
-            s1 = push( root->right, s1 );
-            s2 = push( cur->right, s2 );
-         }
-      }
+      perror( "in.txt" );
+      errno = 0;
+      return 1;
    }
-   return t;
+
+
 }
 
-void open_brackets( tree *root )
+void copy_tree( tree *t, tree *copy )
 {
-   if ( root )
+   if ( !t  ) copy = NULL;
+   else
    {
-      stack *s = NULL;
-      s = push( root, s );
-      char z = 0;
-      for ( ; s; )
-      {
-         push( &root, &s );
-         z = root->el;
-         if ( z == '+' || z == '-' )
-         {
-            if ( root->left->el == '*' && root->right->el == '*' )
-               if ( root->left->left->el == root->right->left->el )
-               {
-                  root->right = new tree( z, root->left->right, root->right->right );
-                  root->el = '*';
-                  root->left = root->left->left;
-               }
-               else if ( root->left->right->el == root->right->right->el )
-               {
-                  root->left = new tree( z, root->left->left, root->right->left );
-                  root->el = '*';
-                  root->right = root->left->right;
-               }
-            s = push( root->right, push( root->left, s ) );
-         }
-         else if ( root->el == '*' )
-            s = push( root->right, push( root->left, s ) );
-      }
+      copy->elem = t->elem;
+      copy_tree( t->left, copy->left );
+      copy_tree( t->right, copy->right );
    }
 }
 
-void PreOrder( tree *root, FILE **fout )
-{
-   if ( root )
-   {
-      stack *s = new stack( root );
-      for ( ; s; )
-      {
-         push( &root, &s );
-         if ( fout ) fprintf_s( *fout, "%c ", root->el );
-         if ( root->right ) s = push( root->right, s );
-         if ( root->left ) s = push( root->left, s );
-      }
-   }
-}
+//tree *tree::copy( )
+//{
+//   tree *t = NULL;
+//   if ( this == NULL ) return NULL;
+//   t->elem = elem;
+//   t->left = left.copy( );
+//   t->right = right.copy( );
+//}
 
-void InOrder( tree *root, FILE **fout )
-{
-   if ( root )
-   {
-      bool IsProcessed = true;
-      stack *s = NULL;
-      for ( ; IsProcessed; )
-      {
-         for ( ; root; s = push( root, s ), root = root->left );
-         if ( push( &root, &s ) )
-         {
-            if ( fout ) fprintf_s( *fout, "%c ", root->el );
-            root = root->right;
-         }
-         else
-            IsProcessed = false;
-      }
-   }
-}
-
-void PostOrder( tree *root, FILE **fout )
-{
-   int count = 0;
-   if ( root )
-   {
-      UCHAR flag = 0;
-      stack *s = NULL;
-      do
-      {
-         for ( ; root; )
-         {
-            s = push( root, s, 0 );
-            root = root->left;
-         }
-         if ( s )
-         {
-            do
-            {
-               if ( push( &root, &s, &flag ) && flag )
-                  if ( fout ) fprintf_s( *fout, "%c ", root->el );
-            } while ( flag && s );
-            if ( !flag )
-            {
-               s = push( root, s, 1 );
-               root = root->right;
-            }
-         }
-      } while ( s );
-   }
-}
-
-stack *push( tree *x, stack *st, UCHAR mark )
-{
-   return new stack( x, st, mark );
-}
-
-bool push( tree **x, stack **st, UCHAR *mark )
-{
-   if ( *st )
-   {
-      if ( x ) *x = (*st)->el;
-      if ( mark ) *mark = (*st)->mark;
-      *st = (*st)->next;
-      return true;
-   }
-   return false;
-}
+//void tree::bracketing( tree *root )
+//{
+//   if ( root )
+//   {
+//      stack *s = NULL;
+//      s = push( root, s );
+//      char z = 0;
+//      for ( ; s; )
+//      {
+//         push( &root, &s );
+//         z = root->elem;
+//         if ( z == '+' || z == '-' )
+//         {
+//            if ( root->left->elem == '*' && root->right->elem == '*' )
+//               if ( root->left->left->elem == root->right->left->elem )
+//               {
+//                  root->right = new tree( z, root->left->right, root->right->right );
+//                  root->elem = '*';
+//                  root->left = root->left->left;
+//               }
+//               elemse if ( root->left->right->elem == root->right->right->elem )
+//               {
+//                  root->left = new tree( z, root->left->left, root->right->left );
+//                  root->elem = '*';
+//                  root->right = root->left->right;
+//               }
+//            s = push( root->right, push( root->left, s ) );
+//         }
+//         elemse if ( root->elem == '*' )
+//            s = push( root->right, push( root->left, s ) );
+//      }
+//   }
+//}
+//
+//void tree::pre_order( tree *root, FILE **fout )
+//{
+//   if ( root )
+//   {
+//      stack *s = new stack( root );
+//      for ( ; s; )
+//      {
+//         push( &root, &s );
+//         if ( fout ) fprintf_s( *fout, "%c ", root->elem );
+//         if ( root->right ) s = push( root->right, s );
+//         if ( root->left ) s = push( root->left, s );
+//      }
+//   }
+//}
+//
+//void tree::in_order( tree *root, FILE **fout )
+//{
+//   if ( root )
+//   {
+//      bool IsProcessed = true;
+//      stack *s = NULL;
+//      for ( ; IsProcessed; )
+//      {
+//         for ( ; root; s = push( root, s ), root = root->left );
+//         if ( push( &root, &s ) )
+//         {
+//            if ( fout ) fprintf_s( *fout, "%c ", root->elem );
+//            root = root->right;
+//         }
+//         elemse
+//            IsProcessed = false;
+//      }
+//   }
+//}
+//
+//void tree::post_order( tree *root, FILE **fout )
+//{
+//   int count = 0;
+//   if ( root )
+//   {
+//      UCHAR flag = 0;
+//      stack *s = NULL;
+//      do
+//      {
+//         for ( ; root; )
+//         {
+//            s = push( root, s, 0 );
+//            root = root->left;
+//         }
+//         if ( s )
+//         {
+//            do
+//            {
+//               if ( push( &root, &s, &flag ) && flag )
+//                  if ( fout ) fprintf_s( *fout, "%c ", root->elem );
+//            } while ( flag && s );
+//            if ( !flag )
+//            {
+//               s = push( root, s, 1 );
+//               root = root->right;
+//            }
+//         }
+//      } while ( s );
+//   }
+//}
+//
+//stack *push( tree *x, stack *st, UCHAR mark )
+//{
+//   return new stack( x, st, mark );
+//}
+//
+//bool push( tree **x, stack **st, UCHAR *mark )
+//{
+//   if ( *st )
+//   {
+//      if ( x ) *x = (*st)->elem;
+//      if ( mark ) *mark = (*st)->mark;
+//      *st = (*st)->next;
+//      return true;
+//   }
+//   return false;
+//}
--- PZ3/tree.h
+++ PZ3/tree.h
@@ -5,33 +5,19 @@
 
 struct tree
 {
-   char el;
+   char elem;
    tree *left, *right;
 
-   tree( char _el = 0, tree *_left = NULL, tree *_right = NULL ) :
-      el( _el ), left( _left ), right( _right ) { }
+   tree( char _elem = 0, tree *_left = NULL, tree *_right = NULL ) :
+      elem( _elem ), left( _left ), right( _right ) { }
+
+   //tree *copy( );
+   void bracketing( );
+   int input( FILE *f );
+   void pre_order( FILE *f );
+   void in_order( FILE *f );
+   void post_order( FILE *f );
+   //void push( char c, bool place);
 };
 
-tree *copy_tree( tree *root );
-
-void open_brackets( tree *root );
-
-void PreOrder( tree *root, FILE **fout );
-
-void InOrder( tree *root, FILE **fout );
-
-void PostOrder( tree *root, FILE **fout );
-
-struct stack
-{
-   tree *el;
-   stack *next;
-   int mark;
-
-   stack( tree *_el = NULL, stack *_stack = NULL, int _mark = 0 ) :
-      el( _el ), next( _stack ), mark( _mark ) {}
-};
-
-stack *push( tree *x, stack *st, UCHAR mark = 0 );
-
-bool push( tree **x, stack **st, UCHAR *mark = NULL );
+void copy_tree( tree *t, tree *copy );
